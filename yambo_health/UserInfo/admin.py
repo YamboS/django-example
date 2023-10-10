85c2480b8b4c1e2921deddb4c664471aa7b0e32e
Sam <syambo@yahoo.com>
2023-10-09 22:20:34 -0400

    Added models to admin view Reworked models

--- yambo_health/UserInfo/admin.py
+++ yambo_health/UserInfo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import BloodPressure,User,Weight
 # Register your models here.
+admin.site.register(BloodPressure)
+admin.site.register(User)
+admin.site.register(Weight)
--- yambo_health/UserInfo/models.py
+++ yambo_health/UserInfo/models.py
@@ -1,38 +1,33 @@
 from django.db import models
 
 class User(models.Model):
+    userId = models.AutoField(primary_key=True)
     datetime = models.DateTimeField(auto_now_add=True)
-    username = models.CharField()
-    password = models.CharField()
-    userId = models.IntegerField(primary_key=True)
-    bpId = models.ForeignKey("BloodPressure",on_delete=models.CASCADE,)
-    weightId = models.ForeignKey("Weight",on_delete=models.CASCADE,)
-
+    user = models.CharField(max_length=15)
+    password = models.CharField(max_length=15)
+    bpId = models.IntegerField(null=True)
+    weightId = models.IntegerField(null=True)
 
     def __str__(self):
-        return self.name
+        return self.user
 
 class BloodPressure(models.Model):
-    bpId = models.IntegerField(primary_key=True)
-    systolicPress = models.IntegerField()
-    diastolicPress = models.IntegerField()
-    bpm = models.IntegerField()
-    userId = models.ForeignKey("User",on_delete=models.PROTECT,)
-    datetime = models.DateTimeField(auto_now_add=True)
+    userId = models.ForeignKey("User",on_delete=models.CASCADE,null=True)
 
+    systolicPress = models.IntegerField(null=True)
+    diastolicPress = models.IntegerField(null=True)
+    bpm = models.IntegerField(null=True)
+    datetime = models.DateTimeField(auto_now_add=True,)
 
-    def __str__(self):
-        return self.name
 
 class Weight(models.Model):
-    weightLb = models.DecimalField()
-    bmi = models.DecimalField(null=True, blank=True)
+    userId = models.ForeignKey("User",on_delete=models.CASCADE,null=True)
+
+    weightLb = models.DecimalField(max_digits=4,decimal_places=1)
+    bmi = models.DecimalField(null=True, blank=True,max_digits=4,decimal_places=2)
     datetime = models.DateTimeField(auto_now_add=True)
-    weightId = models.IntegerField(primary_key=True)
-    userId = models.ForeignKey("User",on_delete=models.PROTECT,)
 
 
-    def __str__(self):
-        return self.name
+
 
 
