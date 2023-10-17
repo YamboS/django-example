4996e2c8bfe78481ce976b409a209b24ca7f1863
Sam <syambo@yahoo.com>
2023-10-17 18:18:56 -0400

    Removed old commented out code

--- yambo_health/UserInfo/views.py
+++ yambo_health/UserInfo/views.py
@@ -1,58 +1,7 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from rest_framework import generics
 from .models import Patient,BloodPressure,Weight
 from .serializers import UserSerializer,BloodPressureSerializer,WeightSerializer
 
-
-# @api_view(['GET', 'PUT', 'DELETE'])
-# def user_details(request, pk):
-#     """
-#     Retrieve, update or delete a user.
-#     """
-#     try:
-#         user = Patient.objects.get(pk=pk)
-#
-#     except Patient.DoesNotExist:
-#         return Response(status=status.HTTP_404_NOT_FOUND)
-#
-#     if request.method == 'GET':
-#         serializer = UserSerializer(user)
-#         return Response(serializer.data)
-#
-#     elif request.method == 'PUT':
-#         serializer = UserSerializer(user, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#     elif request.method == 'DELETE':
-#         user.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
-#
-# @api_view(['GET','POST'])
-# def all_user_details(request):
-#         """
-#         Retrieve, update or delete a user.
-#         """
-#         user = Patient.objects.all()
-#         serializer = UserSerializer(user,many=True)
-#         return Response(serializer.data)
-
-
-# @api_view(['POST'])
-# def create_user(request):
-#         """
-#         CREATE a user.
-#         """
-#         serializer = UserSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
 
 
 class UserList(generics.ListCreateAPIView):
